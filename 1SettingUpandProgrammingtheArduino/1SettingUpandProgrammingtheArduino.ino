403c48b0712cdb4b01e92c4aa7c0ec4c7d6b9fdc
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-31 10:08:51 -0600

    first commit

---
+++ 1SettingUpandProgrammingtheArduino/1SettingUpandProgrammingtheArduino.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);
+
+}
+
+void loop() {
+
+// turn LED on
+digitalWrite(13,HIGH);
+//delay command
+delay(1000);
+//turn LED off
+digitalWrite(13,LOW);
+//delay command
+delay(1000);
+
+}
