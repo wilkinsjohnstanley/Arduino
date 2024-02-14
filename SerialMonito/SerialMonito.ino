d22f8c61019260893f218c7d09f4f5760e70cb32
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-14 11:31:18 -0600

    The serial monitor is Arduinos versions of an Output Terminal

---
+++ SerialMonito/SerialMonito.ino
@@ -0,0 +1,13 @@
+int j=1;
+int waitT=750;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println(j);
+j=j+1;
+delay(waitT);
+}
