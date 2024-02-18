0348b277bf3d0128af43acd89819ff86a36f8c9a
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-17 19:54:35 -0600

    RGBs are four pronged LEDs

---
+++ 26RGB/26RGB.ino
@@ -0,0 +1,14 @@
+int Blue=A3;
+int Green=A4;
+int Red=A5;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(Blue, OUTPUT);
+pinMode(Green, OUTPUT);
+pinMode(Red, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(Green, HIGH);
+}
